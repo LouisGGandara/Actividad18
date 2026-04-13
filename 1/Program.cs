94b290e8504ebadcbddf40a5a199ea696556f0e0
Louis Gandara <crazy@LAPTOP-ULL8SV7B>
2026-04-13 13:35:57 -0600

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿CuentaBancaria cuenta1 = new CuentaBancaria(500);
+CuentaBancaria cuenta2 = new CuentaBancaria(-100);
+
+Console.WriteLine(cuenta1.Saldo); 
+Console.WriteLine(cuenta2.Saldo); 
+
+class CuentaBancaria {
+    private double saldo;
+
+    public CuentaBancaria(double saldo)  // Probando con el constructor para poder visualizarlo
+    {
+        this.saldo = saldo;
+    }
+
+    public string Saldo
+    {
+        get
+        {
+            if (saldo > 0)
+            {
+                return saldo.ToString();
+            }
+            else
+            {
+                return "Saldo no disponible.";
+            }
+        }
+    }
+}
+
---
+++ 2/Program.cs
@@ -0,0 +1,29 @@
+﻿Estudiante e1 = new Estudiante();
+e1.Edad = 25;
+Console.WriteLine("e1.Edad es: " + e1.Edad);
+Console.WriteLine($"e1.edad es: ...Inaccesible por su nivel de protección...");
+
+Estudiante e2 = new Estudiante();
+e2.Edad = 250;
+Console.WriteLine($"e2.Edad es: {e2.Edad}");
+Console.WriteLine("e2.edad es: ...Inaccesible por su nivel de protección...");
+
+
+class Estudiante
+{
+    private int edad;
+
+    public int Edad
+    {
+
+        // Aquí no es necesario un constructor porque int se asigna 0 por default
+        get
+        { if (edad < 0 || edad > 100)
+            {
+                return 0;
+            }
+            else { return edad; }
+        }
+        set { edad = value; }
+    }
+}
